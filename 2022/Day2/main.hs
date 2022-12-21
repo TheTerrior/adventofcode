9c282d833ea96fe0c39778b19e332d048f69b209
TheTerrior <werbird10@gmail.com>
2022-12-21 15:48:57 -0800

    Continued Day2

--- 2022/Day2/main.hs
+++ 2022/Day2/main.hs
@@ -1,4 +1,25 @@
+import System.IO
+import Control.Monad
+import Data.Binary (Binary(putList))
+import Control.Monad.Trans.Accum (accum)
 
+
+
+
+
+
+-- Split a list of elements into groups of 3
+toThrees :: [a] -> [[a]] -> [[a]]
+toThrees (x:y:z:xs) accum = toThrees xs $ [x, y, z] : accum
+toThrees xs accum = accum
+
+-- Main entry point
 main :: IO ()
 main = do
-    putStrLn "hi"
+    handle <- openFile "input.txt" ReadMode
+    contents <- hGetContents handle
+    let contentLines = lines contents
+    let test = toThrees contentLines []
+    print test
+    --putStrLn contents
+
