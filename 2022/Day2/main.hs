74f22ad1cb37fea0fcf4b9166e7e10429aceebbc
TheTerrior <werbird10@gmail.com>
2022-12-21 21:17:10 -0800

    Finished Day2 with Haskell

--- 2022/Day2/main.hs
+++ 2022/Day2/main.hs
@@ -6,19 +6,10 @@ import Control.Monad
 
 
 
--- Split a list of elements into groups of 3
---processThrees :: [a] -> [(a, a)] -> [(a, a)]
---processThrees :: [[a]] -> [((a, a), (a, a), (a, a))] -> [((a, a), (a, a), (a, a))]
---processThrees (line0:line1:line2:xs) accum = processThrees xs $ accum ++ [(lineToGame line0, lineToGame line1, lineToGame line2)]
---processThrees xs accum = accum
-
---lineToGame :: [a] -> (a, a)
---lineToGame [x,y,z] = (x,z)
-
 
 -- Maps a set of lines to simple tuples representing games
 linesToGames :: [String] -> [(Char,Char)]
-linesToGames xs = map (\[x,y,z] -> (x,z)) xs
+linesToGames = map (\[x,y,z] -> (x,z))
 
 
 -- Calculate the score of a single game
@@ -39,9 +30,32 @@ gameScore (x,y)
     | otherwise = 0
 
 
+-- Calculate the score of a single game
+gameScoreNew :: (Char,Char) -> Int
+gameScoreNew (x,y)
+    | x == 'A' && y == 'X' = 3 -- 3 + 0
+    | x == 'A' && y == 'Y' = 4 -- 1 + 3
+    | x == 'A' && y == 'Z' = 8 -- 2 + 6
+
+    | x == 'B' && y == 'X' = 1 -- 1 + 0
+    | x == 'B' && y == 'Y' = 5 -- 2 + 3
+    | x == 'B' && y == 'Z' = 9 -- 3 + 6
+
+    | x == 'C' && y == 'X' = 2 -- 2 + 0
+    | x == 'C' && y == 'Y' = 6 -- 3 + 3
+    | x == 'C' && y == 'Z' = 7 -- 1 + 6
+
+    | otherwise = 0
+
+
 -- Take in a list of games and returns a score
 linesToScore :: [String] -> Int
 linesToScore xs = foldr ((+) . gameScore) 0 $ linesToGames xs
+
+
+-- Take in a list of games and returns a score
+linesToScoreNew :: [String] -> Int
+linesToScoreNew xs = foldr ((+) . gameScoreNew) 0 $ linesToGames xs
 
 
 -- Main entry point
@@ -50,7 +64,7 @@ main = do
     handle <- openFile "input.txt" ReadMode
     contents <- hGetContents handle
     let contentLines = lines contents
-    let test = linesToScore contentLines
-    print test
+    print . linesToScore $ contentLines
+    print . linesToScoreNew $ contentLines
     --putStrLn contents
 
