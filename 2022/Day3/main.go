c7286dd279e6fe1ec44e4c4d64aaa1bfe9c11077
TheTerrior <werbird10@gmail.com>
2022-12-27 01:16:48 -0800

    Completed Pt1 of Day3

--- 2022/Day3/main.go
+++ 2022/Day3/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 // Convert a char into a priority
-func charToPriority(char rune) int8 {
-	convert := int8(char) - 96
+func charToPriority(char rune) int {
+	convert := int(char) - 96
 	if convert < 0 {
 		convert = convert + 58
 	}
@@ -25,14 +25,37 @@ func main() {
 	text := string(dat)
 	textLines := strings.Split(text, "\n")
 
+	total := 0
+
+	// for each line
 	for i := 0; i < len(textLines); i++ {
-		fmt.Println(textLines[i])
+		seen := map[byte]bool{}
+		thisLine := textLines[i]
+		firstHalf := thisLine[:len(thisLine) / 2]
+		secondHalf := thisLine[len(thisLine) / 2:]
+
+		// for each char in the first half
+		for j := 0; j < len(firstHalf); j++ {
+			seen[firstHalf[j]] = true
+		}
+
+		// for each char in the second half
+		for j := 0; j < len(secondHalf); j++ {
+			if seen[secondHalf[j]] {
+				//fmt.Println("Found it: ", string(rune(secondHalf[j])), " on line ", i)
+				total += charToPriority(rune(secondHalf[j]))
+				break
+			}	
+		}
+
 	}
 
-	fmt.Println(charToPriority('a'))
-	fmt.Println(charToPriority('z'))
-	fmt.Println(charToPriority('A'))
-	fmt.Println(charToPriority('Z'))
+	fmt.Println(total)
+
+	//fmt.Println(charToPriority('a'))
+	//fmt.Println(charToPriority('z'))
+	//fmt.Println(charToPriority('A'))
+	//fmt.Println(charToPriority('Z'))
 
 
 }
