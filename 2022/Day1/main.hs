d019ad8cfacf82f34bc5b44a167f8a86407bec9c
TheTerrior <werbird10@gmail.com>
2023-05-11 01:25:49 -0700

    Reformatted Day1 main.hs

--- 2022/Day1/main.hs
+++ 2022/Day1/main.hs
@@ -1,9 +1,10 @@
 import System.IO
-import Data.List
+import Data.List ( sort )
 
 
+-- Given a set of lines, returns the sorted sums of the groups of calories
 groupCalories :: [String] -> [Int]
-groupCalories lis = reverse . Data.List.sort $ groupTail lis [] []
+groupCalories lis = reverse . sort $ groupTail lis [] []
     where
         groupTail :: [String] -> [Int] -> [Int] -> [Int]
         groupTail [] [] accum = accum
@@ -12,9 +13,12 @@ groupCalories lis = reverse . Data.List.sort $ groupTail lis [] []
             | x == "" = groupTail xs [] (accum ++ [sum buf])
             | otherwise = groupTail xs (buf ++ [read x :: Int]) accum
 
+
+-- Returns the first three elements from the given list
 topThree :: [Int] -> [Int]
 topThree (x:y:z:xs) = [x, y, z]
 topThree _ = []
+
 
 -- Main entry point
 main :: IO ()
