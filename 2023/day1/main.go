41682d88a7a17a7b8c013f274caf58cfccafb65e
TheTerrior <werbird10@gmail.com>
2024-02-09 02:36:15 -0800

    Revised Day1 and finished Part2

--- 2023/day1/main.go
+++ 2023/day1/main.go
@@ -6,62 +6,115 @@ import "strings"
 import "strconv"
 
 
-// Grabs the first int rune from a string
-func getFirst(line *string) (string, error) {
-    for _, c := range *line {
-        var _, err = strconv.Atoi(string(c))
-        if err != nil {
-            continue
-        }
-        return string(c), nil
-    }
-    return "", fmt.Errorf("no int in string")
+var numberMap = map[string]int{
+    "one":   1,
+    "two":   2,
+    "three": 3,
+    "four":  4,
+    "five":  5,
+    "six":   6,
+    "seven": 7,
+    "eight": 8,
+    "nine":  9,
 }
 
 
-// Grabs the last int rune from a string
-func getLast(line *string) (string, error) {
-    var sliced = []rune(*line)
-    var length = len(sliced)
-    for i, _ := range sliced {
-        var c = sliced[length - i - 1]
-        var _, err = strconv.Atoi(string(c))
-        if err != nil {
-            continue
+func retFirst(line []rune) int {
+    for i := range line {
+        var n, err = strconv.Atoi(string(line[i]))
+        if err == nil {
+            return n //current char is an int
         }
-        return string(c), nil
+        for str, j := range numberMap {
+            var res = strings.HasPrefix(string(line[i:]), str)
+            if res {
+                return j //number showed up as word
+            }
+        }
     }
-    return "", fmt.Errorf("no int in string")
+    return 0
+}
+
+
+func retLast(line []rune) int {
+    for i := range line {
+        i = len(line) - i - 1
+        var n, err = strconv.Atoi(string(line[i]))
+        if err == nil {
+            return n //current char is an int
+        }
+        for str, j := range numberMap {
+            var res = strings.HasPrefix(string(line[i:]), str)
+            if res {
+                return j //number showed up as word
+            }
+        }
+    }
+    return 0
+}
+
+
+func retFirsta(line []rune) int {
+    for i := range line {
+        var n, err = strconv.Atoi(string(line[i]))
+        if err == nil {
+            return n //current char is an int
+        }
+    }
+    fmt.Println("BAD")
+    return 0
+}
+
+
+func retLasta(line []rune) int {
+    for i := range line {
+        i = len(line) - i - 1
+        var n, err = strconv.Atoi(string(line[i]))
+        if err == nil {
+            return n //current char is an int
+        }
+    }
+    fmt.Println("BAD")
+    return 0
+}
+
+
+
+func part1(text []byte) {
+    var totalSum = 0
+    var splitted = strings.Split(string(text), "\n")
+    for _, line := range splitted {
+        var runed = []rune(line)
+        var fst = retFirsta(runed)
+        var snd = retLasta(runed)
+
+        totalSum += 10 * fst + snd
+    }
+    fmt.Printf("%v\n", totalSum)
+}
+
+
+func part2(text []byte) {
+    var totalSum = 0
+    var splitted = strings.Split(string(text), "\n")
+    for _, line := range splitted {
+        var runed = []rune(line)
+        var fst = retFirst(runed)
+        var snd = retLast(runed)
+
+        totalSum += 10 * fst + snd
+    }
+    fmt.Printf("%v\n", totalSum)
 }
 
 
 func main() {
-    //fmt.Println("hello")
     var text, err = os.ReadFile("input.txt")
     if err != nil {
         panic(err)
     }
-
-    var totalSum = 0
-
-    var splitted = strings.Split(string(text), "\n")
-    for _, line := range splitted {
-        var fst, err = getFirst(&line)
-        if err != nil {
-            continue
-        }
-
-        var snd string
-        snd, err = getLast(&line)
-        if err != nil {
-            continue
-        }
-
-        var res, _ = strconv.Atoi(fst + snd)
-        totalSum += res
-    }
-
-    fmt.Printf("%v\n", totalSum)
+    part1(text)
+    part2(text)
 }
 
 
