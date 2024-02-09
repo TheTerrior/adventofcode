e8ac5ac91db971a3c819a4797120d1a554e3e2ca
TheTerrior <werbird10@gmail.com>
2024-02-09 04:21:07 -0800

    Improved Haskell Day1

--- 2023/day1/main.hs
+++ 2023/day1/main.hs
@@ -1,6 +1,7 @@
 import System.IO
 import Data.List
 import Data.Char
+import Data.Maybe
 
 
 parseOne :: [String] -> Int
@@ -20,36 +21,20 @@ parseOne lines = foldr (\x acc -> acc + 10 * (getFirst x) + (getSecond x)) 0 lin
 parseTwo :: [String] -> Int
 parseTwo lines = foldr (\x acc -> acc + 10 * (getFirst x) + (getSecond x)) 0 lines
     where
+        names = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
         getFirst :: String -> Int
         getFirst str
             | elem (head str) "0123456789" = digitToInt (head str)
-            | isPrefixOf "zero" str =  0
-            | isPrefixOf "one" str =   1
-            | isPrefixOf "two" str =   2
-            | isPrefixOf "three" str = 3
-            | isPrefixOf "four" str =  4
-            | isPrefixOf "five" str =  5
-            | isPrefixOf "six" str =   6
-            | isPrefixOf "seven" str = 7
-            | isPrefixOf "eight" str = 8
-            | isPrefixOf "nine" str =  9
+            | elem str names = fromJust $ elemIndex str names
             | otherwise = getFirst (tail str)
 
         getSecond :: String -> Int
         getSecond str
             | elem (last str) "0123456789" = digitToInt (last str)
-            | isSuffixOf "zero" str =  0
-            | isSuffixOf "one" str =   1
-            | isSuffixOf "two" str =   2
-            | isSuffixOf "three" str = 3
-            | isSuffixOf "four" str =  4
-            | isSuffixOf "five" str =  5
-            | isSuffixOf "six" str =   6
-            | isSuffixOf "seven" str = 7
-            | isSuffixOf "eight" str = 8
-            | isSuffixOf "nine" str =  9
+            | elem str names = fromJust $ elemIndex str names
             | otherwise = getSecond (init str)
-        
+
 
 main :: IO ()
 main = do
